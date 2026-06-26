538dd015fecc3f5566770247c250e9bd188c4165
Siddharthgitthub <siddharthsingh.1004@gmail.com>
2026-06-26 16:45:09 +0530

    added scroll to top

--- risklens-frontend/src/App.jsx
+++ risklens-frontend/src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Analysis from "./pages/Analysis";
 import Dashboard from "./pages/Dashboard";
-
+import ScrollToTop from "./components/ScrollToTop";
 export default function App(){
   return(
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Landing/>}/>
         <Route path="/analysis" element={<Analysis/>}/>
